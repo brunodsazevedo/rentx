2930ba2d2254114c376d54202b37416ba0f80407
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2022-05-28 15:21:55 -0300

    finalizado aula interface de agendamento

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { RectButtonProps } from 'react-native-gesture-handler';
+
+import {
+  Container,
+  Title,
+} from './styles';
+
+interface Props {
+  title: string;
+  color?: string;
+}
+
+export function Button({
+  title,
+  color,
+  ...rest
+}: Props) {
+  return(
+    <Container color={color} {...rest}>
+      <Title>
+        {title}
+      </Title>
+    </Container>
+  );
+}
---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { BackButton } from '../../components/BackButton';
+import { useTheme } from 'styled-components';
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from './styles';
+
+import ArrowSvg from '../../assets/arrow.svg';
+import { Button } from '../../components/Button';
+
+export function Scheduling() {
+  const theme = useTheme();
+  
+  return(
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+
+        <BackButton
+          color={theme.colors.shape}
+        />
+
+        <Title>
+          Escolha uma {'\n'}
+          data de início e {'\n'}
+          fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false} />
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÈ</DateTitle>
+            <DateValue selected={false} />
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content>
+
+      </Content>
+      
+      <Footer>
+        <Button
+          title="Confirmar"
+        />
+      </Footer>
+    </Container>
+  );
+}
