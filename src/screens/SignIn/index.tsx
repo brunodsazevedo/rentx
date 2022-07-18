66d36571fd3893b442b335f99a2a856c827a4fa7
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2022-07-18 05:42:02 -0300

    Finalizada aula de acessibilidade formulario

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { useTheme } from 'styled-components';
 
 import { Button } from '../../components/Button';
@@ -19,57 +19,64 @@ export function SignIn() {
   const theme = useTheme();
   
   return (
-    <Container>
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent
-      />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        behavior="position"
+        enabled
+      >
+        <Container>
+          <StatusBar
+            barStyle="dark-content"
+            backgroundColor="transparent"
+            translucent
+          />
 
-      <Header>
-        <Title>
-          Estamos {'\n'}
-          quase lá.
-        </Title>
+          <Header>
+            <Title>
+              Estamos {'\n'}
+              quase lá.
+            </Title>
 
-        <SubTitle>
-          Faça seu login para começar {'\n'}
-          uma experiência incrível.
-        </SubTitle>
-      </Header>
+            <SubTitle>
+              Faça seu login para começar {'\n'}
+              uma experiência incrível.
+            </SubTitle>
+          </Header>
 
-      <Form>
-        <Input
-          placeholder="E-mail"
-          iconName="mail"
-          keyboardType="email-address"
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
+          <Form>
+            <Input
+              placeholder="E-mail"
+              iconName="mail"
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
 
-        <PasswordInput
-          iconName="lock"
-          placeholder="Senha"
-        />
-      </Form>
+            <PasswordInput
+              iconName="lock"
+              placeholder="Senha"
+            />
+          </Form>
 
-      <Footer>
-        <Button
-          title="Login"
-          enabled={false}
-          loading={false}
-          onPress={() => {}}
-        />
+          <Footer>
+            <Button
+              title="Login"
+              enabled={false}
+              loading={false}
+              onPress={() => {}}
+            />
 
-        <Button
-          title="Criar conta gratuita"
-          color={theme.colors.background_secondary}
-          light
-          enabled={false}
-          loading={false}
-          onPress={() => {}}
-        />
-      </Footer>
-    </Container>
+            <Button
+              title="Criar conta gratuita"
+              color={theme.colors.background_secondary}
+              light
+              enabled={false}
+              loading={false}
+              onPress={() => {}}
+            />
+          </Footer>
+        </Container>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,10 +1,8 @@
 import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
-import theme from '../../styles/theme';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
-  flex: 1;
   padding: 0 24px;
 
   background-color: ${({ theme }) => theme.colors.background_primary};
