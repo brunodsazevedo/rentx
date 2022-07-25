ba3cdb1e06f6174fc7945d3618328f4c66701dba
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2022-07-25 04:42:58 -0300

    feito validação de cadastro

--- src/screens/SignUp/SignUpSecondStep/index.tsx
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 import { BackButton } from '../../../components/BackButton';
 import { Bullet } from '../../../components/Bullet';
@@ -27,12 +28,36 @@ interface NavigationProps {
   navigate: (screen: string) => void;
 }
 
+interface Params {
+  user: {
+    name: string;
+    email: string;
+    driverLicense: string;
+  }
+}
+
 export function SignUpSecondStep() {
+  const [ password, setPassword ] = useState('');
+  const [ passwordConfirm, setPasswordConfirm ] = useState('');
+
   const navigation = useNavigation<NavigationProps>();
+  const route = useRoute();
   const theme = useTheme();
+
+  const { user } = route.params as Params;
 
   function handleBack() {
     navigation.goBack();
+  }
+
+  function handleRegister() {
+    if(!password || !passwordConfirm) {
+      return Alert.alert('Informe a senha e a confirmação.');
+    }
+
+    if(password !== passwordConfirm) {
+      return Alert.alert('As senhas não são iguais');
+    }
   }
 
   return (
@@ -67,17 +92,22 @@ export function SignUpSecondStep() {
             <PasswordInput
               iconName="lock"
               placeholder="Senha"
+              value={password}
+              onChangeText={setPassword}
             />
 
             <PasswordInput
               iconName="lock"
               placeholder="Repetir senha"
+              value={passwordConfirm}
+              onChangeText={setPasswordConfirm}
             />
           </Form>
 
           <Button
             title="Cadastrar"
             color={theme.colors.success}
+            onPress={handleRegister}
           />
         </Container>
       </TouchableWithoutFeedback>
